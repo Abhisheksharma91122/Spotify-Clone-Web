69cfc54411b7ce895353fa7a3817e5a93a6001d7
Abhishek <abhisheksharma270820@gmail.com>
2024-12-26 23:55:38 +0530

    Make a song playlist using CSS and add songs using JavaScript, and also make a playbar with previous, play, and next buttons.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.log(`Let's write some javascript`);
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:3000/song/");
     let response = await a.text();
+    console.log(response)
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
@@ -17,23 +18,61 @@ async function getSongs() {
     }
     return songs;
 }
-async function main(){
-    let song = await getSongs();
-    console.log(song)
+async function main() {
+    let songs = await getSongs();
+    console.log(songs)
 
-    var audio = new Audio(song[0]);
+    var audio = new Audio(songs[0]);
     // audio.play();
 
 
     const extractSongName = (url) => {
-        const parts = url.split('/').pop(); // Get the last part of the URL
-        const songNameWithExtension = decodeURIComponent(parts);
-        const songName = songNameWithExtension.split('-')[1]?.trim(); // Extract "Aaj Ki Raat"
-        return songName || "Unknown Song";
+        const parts = url.split('/').pop(); 
+        const decodedParts = decodeURIComponent(parts);
+        const [bitrate, songWithMovie] = decodedParts.split('-');
+
+        if (songWithMovie) {
+            const songName = songWithMovie.trim();
+            const movieName = decodedParts.match(/- (.*?) \d+/)?.[1]; 
+            return {
+                songName,
+                movieName: movieName || "Unknown Movie"
+            };
+        }
+
+        return { songName: "Unknown Song", movieName: "Unknown Movie" };
     };
 
-    let songname1 = extractSongName(song[0]);
-    console.log(songname1);
+    // let songname1 = extractSongName(songs[4]);
+    // console.log(songsname1.songName);
+    // console.log(songsname1.movieName);
+
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+
+    for (const song of songs) {
+
+        let name = extractSongName(song);
+        console.log(name.songName);
+        console.log(name.movieName);
+
+
+        songUL.innerHTML = songUL.innerHTML + `<li>
+                            <div class="playbtnimg">
+                                <img class="invert" src="svg/playbutton.svg" alt="">
+                            </div>
+                            <div class="songimg">
+                                <img src="https://c.saavncdn.com/890/Aaj-Ki-Raat-Bass-House-Mix-Hindi-2024-20240905133924-150x150.jpg"
+                                    alt="">
+                            </div>
+                            <div class="songinfo">
+                                <h2>${name.songName} - ${name.movieName}</h2>
+                            </div>
+                        </li>`
+    }
+
+
+
 }
 
 main()
